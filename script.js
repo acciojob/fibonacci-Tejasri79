db5d96ae740ab0de2241b0e08e741778e0ba3945
Tejasri79 <70627567+Tejasri79@users.noreply.github.com>
2024-04-23 09:17:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function fibonacci(num) {
 // your code here
-	let n= 10
-	let f1=0
-	let f2=1
-	if(i=0;i<=n;i++)
-	let f3=f1+f2
-	console.log(f3)
-	
+	if (num <= 0) {
+        return 0; // Fibonacci sequence starts with 0
+    } else if (num === 1) {
+        return 0; // Special case for the first Fibonacci number
+    }
+
+    let a = 0, b = 1;
+    for (let i = 2; i < num; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
 }
 
 module.exports = fibonacci;
